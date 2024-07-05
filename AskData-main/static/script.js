7ec7db12f662870b85bd8b4dd2fb7df1dcf4bc9e
AndreiKharchenko1 <59969267+AndreiKharchenko1@users.noreply.github.com>
2024-07-05 13:42:24 -0400

    Add files via upload

--- AskData-main/static/script.js
+++ AskData-main/static/script.js
@@ -1,3 +1,51 @@
+function straighten(message) {
+    let newMessage = ""; // Create a new string
+  
+    for (let i = 0; i < message.length; i++) {
+      let char = message[i]; // Get the current character
+  
+      // Check for backtick and asterisk
+      if (char !== '`' && char !== '*') {
+        newMessage += char; 
+      }
+  
+      if (i <= message.length - 3 && 
+          message[i].toLowerCase() === 's' &&
+          message[i + 1].toLowerCase() === 'q' &&
+          message[i + 2].toLowerCase() === 'l') {
+        newMessage += "Following SQL code."; 
+        i += 2;
+      }
+    }
+  
+    return newMessage;
+  }
+  function prettifyMe(textContent) {
+    // Replace '***' with HTML <b><i> tags for bold and italic text
+    textContent = textContent.replace(/\*\*\*(.*?)\*\*\*/g, '<b><i>$1</i></b>');
+
+    // Replace '**' with HTML <b> tags for bold text
+    textContent = textContent.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+
+    // Replace '*' with HTML <i> tags for italic text
+    textContent = textContent.replace(/\*(.*?)\*/g, '<i>$1</i>');
+
+    // Replace triple backticks ''' with <pre><code> tags for code blocks
+    textContent = textContent.replace(/'''(.*?)'''/gs, '<pre><code>$1</code></pre>');
+
+    // Replace triple backticks ``` with <pre><code> tags for code blocks
+    textContent = textContent.replace(/```(.*?)```/gs, '<pre><code>$1</code></pre>');
+
+    // Custom formatting for sections
+    textContent = textContent.replace(/(Data Professional Question:)/g, '<span style="color: blue; font-size: 1.2em;">$1</span>');
+    textContent = textContent.replace(/(Chatbot Response:)/g, '<span style="color: green; font-size: 1.2em;">$1</span>');
+    textContent = textContent.replace(/(Additional Tips:)/g, '<span style="color: red; font-size: 1.2em;">$1</span>');
+
+    // Custom formatting for bullet points
+    textContent = textContent.replace(/(\* .+?)/g, '<span style="color: brown; font-size: 1em;">$1</span>');
+
+    return textContent;
+}
 function sendMessage() {
     var userInput = document.getElementById('user_input').value;
 
@@ -33,9 +81,10 @@ function sendMessage() {
         botMessage.appendChild(icon);
 
         var text = document.createElement('span');
-        text.innerHTML = '<span style="color: #089799;">AskData: </span><span style="color: #5e3101;">' + aiResponse + '</span>';
+        var prettyResponse = prettifyMe(aiResponse);
+        text.innerHTML = '<span style="color: #089799;">AskData: </span>' + prettyResponse;
         botMessage.appendChild(text);
-        
+
         chatBox.appendChild(botMessage);
 
         document.getElementById('user_input').value = '';
