5e119c70a5423c18abee79be963d3995985005fa
AndreiKharchenko1 <59969267+AndreiKharchenko1@users.noreply.github.com>
2024-07-19 17:15:30 -0400

    Update AskDataDisplay.py

--- AskDataDisplay.py
+++ AskDataDisplay.py
@@ -10,6 +10,10 @@ from DataWrangler import DataWrangler
 from dotenv import load_dotenv
 load_dotenv()
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
+
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
 
 if not GEMINI_API_KEY:
